e68ca4d633092cf2d337f586689e2ed9ad3f9e80
אביטל כהנא <116591046+AvitalHoro@users.noreply.github.com>
2024-06-07 16:30:55 +0300

    Update ParamsQueries.sql

--- level B/ParamsQueries.sql
+++ level B/ParamsQueries.sql
@@ -15,3 +15,52 @@ HAVING
     required=true hint="enter a minimum number of copies">
 ORDER BY 
     NumberOfCopies &<name="sort accoring to" checkbox="desc, ">;
+
+--Which books were specifically ordered
+SELECT
+  bo.orderid,
+  ord.orderdate AS OrderDate,
+  b.bookname AS BookName,
+  b.bookpublisher,
+  r.readerName
+FROM
+  bookordering bo  
+JOIN
+  ordering ord ON bo.orderid = ord.orderid
+JOIN
+  book b ON bo.bookid = b.bookid
+JOIN
+  reader r ON bo.readerNumber=r.readerNumber
+WHERE
+  bo.orderid = &<name="order number" hint="what your order number?">
+ORDER BY
+  b.bookname;
+
+--Number of orders that suppliers delivered in a specific date range
+SELECT
+  s.supName,
+  s.supphone,
+  COUNT(o.orderid) AS number_of_delivered_orders
+FROM
+  ordering o
+JOIN
+  supplier s ON o.supid = s.supid
+WHERE
+  o.orderdate BETWEEN TO_DATE('&startDate', 'DD-MM-YYYY') AND TO_DATE('&endDate', 'DD-MM-YYYY')
+  AND o.status = 'Delivered'
+GROUP BY
+  s.supName, s.sphone
+ORDER BY
+  number_of_delivered_orders DESC;
+  
+--Update book orders under a specific order number
+UPDATE bookOrdering
+SET orderID = &<name="order_number" hint="The order in which the books you selected were ordered">
+WHERE bookID IN (
+    &<name="books_name" 
+      list="SELECT bor.bookID, b.bookName 
+            FROM bookOrdering bor 
+            JOIN book b ON bor.bookID = b.bookID 
+            WHERE bor.orderID IS NULL" 
+      multiselect="yes">
+);
