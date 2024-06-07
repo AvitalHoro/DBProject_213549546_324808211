7c2eb9d37cd92915292635ed37e4ec20db2a1637
אביטל כהנא <116591046+AvitalHoro@users.noreply.github.com>
2024-06-07 15:35:38 +0300

    Update AlterTable.sql

--- level B/AlterTable.sql
+++ level B/AlterTable.sql
@@ -1,7 +1,9 @@
+--Add the ON DELETE CASCADE constraint to ordering so we can delete a librarian
 ALTER TABLE ordering DROP CONSTRAINT SYS_C008809;
 
 ALTER TABLE ordering
 ADD CONSTRAINT SYS_C008809
 FOREIGN KEY (LuserName) REFERENCES librarian(LuserName) ON DELETE CASCADE;
 
+--remove not null constraint from librarian's name
 ALTER TABLE librarian MODIFY LibrarianName varchar2(40);
